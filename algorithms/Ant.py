9de9f0e8d1930df45971e7130d3ecbee73efa53c
Alexmeri1 <alexander.meriakri@gmail.com>
2025-03-02 22:45:15 -0500

    added things

--- algorithms/Ant.py
+++ algorithms/Ant.py
@@ -5,7 +5,7 @@
 import math
 import random
 from collections import deque
-
+from time import sleep
 import pygame
 # Initialize Pygame
 pygame.init()
@@ -17,14 +17,13 @@ HEIGHT = screen_info.current_h
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Moving Rectangles")
 
-    # Colors
+# Colors
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 BLACK = (0,0,0)
 YELLOW = (255,222,33)
-
 
 center_of_screen_x , center_of_screen_y = WIDTH / 2, HEIGHT / 2
 
@@ -60,29 +59,38 @@ class Food:
 
 
 class Ant:
+
     def __init__(self,angle,x,y):
         self.x = x  #x position on map
         self.y = y  #y position on map
         self.angle = angle #angle ant facing
         self.color = BLACK #color
-        self.speed = 10 #speed of the ant
+        self.speed = 8 #speed of the ant
         self.width, self.height = 10, 20
         # Trail: deque to store (x, y, alpha) with max length
         self.trail = deque(maxlen=1000)  # Adjust maxlen for trail length
         self.turn_cooldown = 0
+        self.sleep_cooldown = 0
+
     def get_rect(self):
         # Return a pygame.Rect representing the ant's bounding box
         return pygame.Rect(self.x, self.y, self.width, self.height)
+
     def get_next_coordinates(self):
         deltaX = math.cos(self.angle * math.pi / 180) * self.speed
         deltaY = math.sin(self.angle * math.pi / 180) * self.speed
         return self.x + deltaX, self.y + deltaY
 
-
     def move(self,food):
+
+        if self.sleep_cooldown > 0:
+            self.sleep_cooldown -= 1
+            return  # Don't move while stopped
+
         if food.collides_with_ant(self) and self.turn_cooldown <= 0:
             print("Food collected! Turning around...")
-            self.angle += 180  # Turn around
+            self.sleep_cooldown = 60
+            self.angle += 180 + random.randint(-50,50)  # Turn around
             self.turn_cooldown = 30  # Set cooldown to prevent immediate re-trigger
 
             # Update cooldown
@@ -93,14 +101,12 @@ class Ant:
         self.trail.append((self.x,self.y,255))
         possible_x,possible_y = self.get_next_coordinates()
 
-        if  (possible_x > WIDTH or possible_x < 0 or possible_y > HEIGHT or possible_y < 0):
+        if (possible_x > WIDTH or possible_x < 0 or possible_y > HEIGHT or possible_y < 0):
             self.angle += 180
             self.x , self.y = self.get_next_coordinates()
 
-
-
-
         self.x, self.y = self.get_next_coordinates()
+
     def draw(self, surface):
 
         for i, (trail_x,trail_y,alpha) in enumerate(reversed(self.trail)):
@@ -114,7 +120,7 @@ class Ant:
 
         pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height))
 
-ants = [Ant(random.randint(1,360),random.randint(1,WIDTH),random.randint(1,HEIGHT)) for _ in range(50)]
+ants = [Ant(random.randint(1,360),random.randint(1,WIDTH),random.randint(1,HEIGHT)) for _ in range(30)]
 foods = [Food(random.randint(1,WIDTH) , random.randint(1,HEIGHT), 50 ) for _ in range(3)]
 
 # Game loop
